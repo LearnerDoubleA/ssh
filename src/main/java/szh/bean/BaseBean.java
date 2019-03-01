d1f4a935fa765c50b44f18a7df7a1023dfb7cd30
= <sunzhehao903109442@outlook.com>
2019-03-01 15:07:56 +0800

    hibernate base bean

---
+++ src/main/java/szh/bean/BaseBean.java
@@ -0,0 +1,56 @@
+package szh.bean;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Calendar;
+
+@MappedSuperclass
+public class BaseBean implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GenericGenerator(name="system-uuid",strategy="uuid")
+    private Long id;
+
+    private Calendar addtime;
+
+    private Calendar updatetime;
+
+    private boolean checkDel = false;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Calendar getAddtime() {
+        return addtime;
+    }
+
+    public void setAddtime(Calendar addtime) {
+        this.addtime = addtime;
+    }
+
+    public Calendar getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Calendar updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public boolean isCheckDel() {
+        return checkDel;
+    }
+
+    public void setCheckDel(boolean checkDel) {
+        this.checkDel = checkDel;
+    }
+}
